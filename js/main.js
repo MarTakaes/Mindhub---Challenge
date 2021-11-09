3e166b4aea41d8c91f307028cefec4dab0cec795
lucas-romer <romerolucas89@gmail.com>
2021-11-09 16:20:59 -0300

    Creamos la funcion renderizarCartas

--- js/main.js
+++ js/main.js
@@ -4,9 +4,30 @@ let init = {
     method: "GET"
 }
 
+function renderizarCartas(lista) {
+    let juguetesBox = document.querySelector(".juguetes_box")
+    lista.forEach(element => {
+        if(element.tipo === 'Juguete'){
+            juguetesBox.innerHTML += `<div class="card" style="width: 18rem;">
+            <img src="${element.imagen}" class="card-img-top" alt="${element.nombre}">
+            <div class="card-body">
+              <h5 class="card-title">${element.nombre}</h5>
+              <p class="card-text">${element.descripcion}</p>
+              <p class="card-text">Precio: $${element.precio}</p>
+              <p class="card-text">Stock: ${element.stock}</p>
+              <a href="" class="btn btn-primary">Comprar</a>
+            </div>
+          </div>`
+        }
+
+    });
+}
+
 fetch(endpoint, init)
     .then(res => res.json())
     .then(data => {
         const articulos = data.response;
-        console.log(articulos)
+        renderizarCartas(articulos)
     })
+
+
