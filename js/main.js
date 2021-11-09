60e9855ed0975c996063134c261214642cfe4cd0
Maca-Acha <achamariamacarena@gmail.com>
2021-11-09 17:31:49 -0300

    funcion Cinco

--- js/main.js
+++ js/main.js
@@ -6,20 +6,6 @@ function detectarCinco(array) {
     });
 }
 
-function filtrarArticulo(array) {
-    let medicamentos = array.filter(e => e.tipo === "Medicamento")
-    console.log(medicamentos)
-    let juguetes = array.filter(e => e.tipo === "Juguete")
-    console.log(juguetes)
-
-    
-
-}
-
-
-
-
-
 let endpoint = `https://apipetshop.herokuapp.com/api/articulos`
 
 let init = {
