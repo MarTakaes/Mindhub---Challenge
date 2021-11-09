f17069b32c3b9581c088cd3240e8ad17fd6defc9
Maca-Acha <achamariamacarena@gmail.com>
2021-11-09 15:26:42 -0300

    Agregamos Js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let endpoint = `https://apipetshop.herokuapp.com/api/articulos`
+
+let init = {
+    method: "GET"
+}
+
+fetch(endpoint, init)
+    .then(res => res.json())
+    .then(data => {
+        const articulos = data.response;
+        console.log(articulos)
+    })
