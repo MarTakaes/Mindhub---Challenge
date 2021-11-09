0b6f7c61042260a2077b4500e38601813149f4ce
Maca-Acha <achamariamacarena@gmail.com>
2021-11-09 16:56:15 -0300

    Funcion detectar 5

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
+function detectarCinco(array) {
+    array.forEach(element => {
+        if (element.stock <= 5) {
+            console.log(element.nombre)
+        }
+    });
+}
+
 function filtrarArticulo(array) {
     let medicamentos = array.filter(e => e.tipo === "Medicamento")
     console.log(medicamentos)
     let juguetes = array.filter(e => e.tipo === "Juguete")
     console.log(juguetes)
+
 }
 
 let endpoint = `https://apipetshop.herokuapp.com/api/articulos`
@@ -16,4 +25,5 @@ fetch(endpoint, init)
     .then(data => {
         const articulos = data.response;
         filtrarArticulo(articulos)
+        detectarCinco(articulos)
     })
