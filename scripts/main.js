19d2502f4b41dbbcdede686ca97186aa212c68d6
Maca-Acha <achamariamacarena@gmail.com>
2021-11-10 18:46:59 -0300

    agregar al carrito Boton

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ function detectarCinco(array) {
 function filtrarElementos(lista) {
     let tipo = document.title.includes('Juguetes') ? "Juguete" : "Medicamento"
     lista.forEach(element => {
-        if(tipo === element.tipo){
+        if (tipo === element.tipo) {
             renderizarCartas(element)
             console.log('primer if')
         }
@@ -21,19 +21,31 @@ function renderizarCartas(element) {
     articulosBox.innerHTML += `<div class="card m-2 shadow p-3 mb-5 bg-body rounded col-sm-6 col-md-4 col-lg-3 col-xl-3.2 col-xxl-2">
             <img src="${element.imagen}" class="card-img-top img-thumbnail" style="max-height:15rem; object-fit: scale-down;" alt="${element.nombre}">
             <div class="card-body d-flex flex-column justify-content-between">
-              <div class="d-flex flex-column justify-content-evenly">
-              <h5 class="card-title">${element.nombre}</h5>
-              <p class="card-text">${element.descripcion}</p>
-              </div>
-              <div class="d-flex flex-column justify-content-end">
-              <p class="card-text">Precio: $${element.precio}</p>
-              <p class="text-danger"> ${element.stock <= 5 ? "ÚLTIMAS UNIDADES!!!" : ""} </p>
-              <a href="" class="btn btn-primary">Añadir al carrito</a>
-              </div>
+                <div class="d-flex flex-column justify-content-evenly">
+                <h5 class="card-title">${element.nombre}</h5>
+                <p class="card-text">${element.descripcion}</p>
+                </div>
+                <div class="d-flex flex-column justify-content-end">
+                <p class="card-text">Precio: $${element.precio}</p>
+                <p class="text-danger"> ${element.stock <= 5 ? "ÚLTIMAS UNIDADES!!!" : ""} </p>
+                <a href="" class="btn btn-primary addToCart" >Añadir al carrito</a>
+                </div>
             </div>
-          </div>`
+            </div>`
 }
 
+const agregarAlCarrito = document.querySelectorAll('.addToCart')
+
+agregarAlCarrito.forEach((addToCartButton) => {
+    addToCartButton.addEventListener("click", agregarAlCarritoClicked())
+})
+
+let carro = []
+
+function agregarAlCarritoClicked(event) {
+    const button = event.target
+    console.log('agregarAlCarritoClicked -> button', button)
+}
 
 let endpoint = `https://apipetshop.herokuapp.com/api/articulos`
 
@@ -48,4 +60,3 @@ fetch(endpoint, init)
         filtrarElementos(articulos)
         detectarCinco(articulos)
     })
-
